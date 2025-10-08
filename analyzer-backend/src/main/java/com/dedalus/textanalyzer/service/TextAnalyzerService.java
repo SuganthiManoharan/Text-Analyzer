33c9036bee2aea94ba5ace23bb0fdc8986c4e2c2
SuganthiManoharan <Suganthi.manoharan@gmail.com>
2025-10-08 18:29:43 +0200

    backend commit

--- analyzer-backend/src/main/java/com/dedalus/textanalyzer/service/TextAnalyzerService.java
+++ analyzer-backend/src/main/java/com/dedalus/textanalyzer/service/TextAnalyzerService.java
@@ -1,28 +1,22 @@
 package com.dedalus.textanalyzer.service;
-
-
-
 import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
 
 @Service
 public class TextAnalyzerService {
 
     private static final Set<Character> VOWELS = Set.of('A', 'E', 'I', 'O', 'U');
     public enum Mode { VOWELS, CONSONANTS }
-
-
     public  Map<Character, Integer> analyze(Mode mode, String text) {
         Map<Character, Integer> result = new HashMap<>();
-        for (char c : text.toUpperCase().toCharArray()) {
-            if (Character.isLetter(c)) {
-                if (mode == Mode.VOWELS && VOWELS.contains(c)) {
-                    result.put(c, result.getOrDefault(c, 0) + 1);
-                } else if (mode == Mode.CONSONANTS && !VOWELS.contains(c)) {
-                    result.put(c, result.getOrDefault(c, 0) + 1);
+        for (char charToAnalze : text.toUpperCase().toCharArray()) {
+            if (Character.isLetter(charToAnalze)) {
+                if (mode == Mode.VOWELS && VOWELS.contains(charToAnalze)) {
+                    result.put(charToAnalze, result.getOrDefault(charToAnalze, 0) + 1);
+                } else if (mode == Mode.CONSONANTS && !VOWELS.contains(charToAnalze)) {
+                    result.put(charToAnalze, result.getOrDefault(charToAnalze, 0) + 1);
                 }
             }
         }
